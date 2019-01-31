a3e3b1ea7be642d8bce2e2312108fb8a6dbb1ce5
Franco <francofwolff@gmail.com>
2019-01-31 18:18:47 -0200

    Update index.jsx

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.reder(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
